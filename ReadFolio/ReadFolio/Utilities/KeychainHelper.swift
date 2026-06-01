ecbf6d9bd6782c709ab8214e718d21c1c2e26c56
ChrisLoc2001 <chris.loc2001@gmail.com>
2026-06-01 17:31:11 +0200

    Eliminati Warnign Vari e errori a livello di sicurezza

---
+++ ReadFolio/ReadFolio/Utilities/KeychainHelper.swift
@@ -0,0 +1,38 @@
+// Utilities/KeychainHelper.swift
+import Foundation
+import Security
+
+enum KeychainHelper {
+    static func save(_ value: String, for key: String) {
+        let data = Data(value.utf8)
+        let query: [CFString: Any] = [
+            kSecClass:           kSecClassGenericPassword,
+            kSecAttrAccount:     key,
+            kSecValueData:       data,
+            kSecAttrAccessible:  kSecAttrAccessibleWhenUnlockedThisDeviceOnly
+        ]
+        SecItemDelete(query as CFDictionary)
+        SecItemAdd(query as CFDictionary, nil)
+    }
+
+    static func load(for key: String) -> String {
+        let query: [CFString: Any] = [
+            kSecClass:       kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecReturnData:  true,
+            kSecMatchLimit:  kSecMatchLimitOne
+        ]
+        var result: AnyObject?
+        SecItemCopyMatching(query as CFDictionary, &result)
+        guard let data = result as? Data else { return "" }
+        return String(decoding: data, as: UTF8.self)
+    }
+
+    static func delete(for key: String) {
+        let query: [CFString: Any] = [
+            kSecClass:       kSecClassGenericPassword,
+            kSecAttrAccount: key
+        ]
+        SecItemDelete(query as CFDictionary)
+    }
+}
